67c13a44df00e7b2003dbdb4fa6d10a068135598
Jackson Mooring <jackson.mooring@gmail.com>
2020-12-29 14:59:33 -0600

    Refactor Code

---
+++ tests/unit/.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('String.split() splits the string on the delimiter', () {
+    var string = 'foo,bar,baz';
+    expect(string.split(','), equals(['foo', 'bar', 'baz']));
+  });
+}
